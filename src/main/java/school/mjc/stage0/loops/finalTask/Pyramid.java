1dd3ef3aa7760c4b2aa08ab110ab8a8e106d5f8f
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-08-01 22:47:38 +0400

    Module4.2 taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,11 +1,29 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
-    public void printPyramid(int cathetusLength) {
-
+    public static void main(String[] args) {
+        new Pyramid().printPyramid(9);
     }
 
-    public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+    public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (j > cathetusLength - i) {
+                    System.out.print(cathetusLength + 1 - j);
+                } else if (j <= cathetusLength - i) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 2; j <= cathetusLength; j++) {
+                if(i<j){
+                    continue;
+                }
+                if(i>=j)
+                {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
     }
 }
